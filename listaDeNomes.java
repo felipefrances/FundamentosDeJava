a12961a2ebe8845d0ff572903e690b19d5b4a9a5
lipefrances <lipefrances@yahoo.com.br>
2023-09-04 19:43:52 -0300

    .

---
+++ listaDeNomes.java
@@ -0,0 +1,30 @@
+//Elaborar um programa que solicite a quantidade de nomes de pessoas a entrar
+//e receba a quantidade indicada.
+//Ao final,o programa deve apresentar todos os nomes fornecidos.
+
+package javafundamentals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class listaDeNomes {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int qtdeParticipantes;
+        List<String> nomes = new ArrayList<>();
+        System.out.print("Digite a quantidade de participantes: ");
+        qtdeParticipantes = scan.nextInt();
+
+        for (int i = 0; i < qtdeParticipantes; i++) {
+            System.out.print("Digite o nome: ");
+            nomes.add(scan.next());
+
+        }
+
+        for(String n: nomes){
+
+        }
+    }
+
+}
